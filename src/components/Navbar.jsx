fb2b83adb0409914f3af6f406ef619c805dd2808
Prachi24599 <prachi24599@gmail.com>
2023-09-10 00:15:15 +0530

    navbar created

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,28 @@
+import { FaShoppingCart } from "react-icons/fa";
+import { NavLink } from "react-router-dom";
+
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <div className="flex flex-row justify-between">
+      <NavLink to="/">
+        <div>
+          <img src="#" alt="logo" />
+        </div>
+      </NavLink>
+      <div>
+        <NavLink>
+          <div>
+            <p>Home</p>
+          </div>
+        </NavLink>
+        <NavLink>
+          <div>
+            <FaShoppingCart />
+          </div>
+        </NavLink>
+      </div>
+    </div>
+  );
 };
 
 export default Navbar;
