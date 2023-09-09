40cf50d6a5e221389e7cb7b297f59ee658a33f0b
Prachi24599 <prachi24599@gmail.com>
2023-09-10 00:32:29 +0530

    API Call to get data

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,28 @@
+import { useEffect, useState } from "react";
+
 const Home = () => {
   const API_URL = "https://fakestoreapi.com/products";
+  const [loading, setLoading] = useState(false);
+  const [posts, setPosts] = useState([]);
 
-  return <div>Home</div>;
+  async function fetchProductData() {
+    setLoading(true);
+    try {
+      const res = await fetch(API_URL);
+      const data = await res.json();
+      console.log(data);
+    } catch (error) {
+      console.log("This is ERROR ", error);
+      setPosts([]);
+    }
+    setLoading(false);
+  }
+
+  useEffect(() => {
+    fetchProductData();
+  }, []);
+
+  return <div>This is Home Page</div>;
 };
 
 export default Home;
