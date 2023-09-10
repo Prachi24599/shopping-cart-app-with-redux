563b5a611981dd13002d56ca42092071839fcaea
Prachi24599 <prachi24599@gmail.com>
2023-09-10 14:21:25 +0530

    cart item created

--- src/components/CartItem.jsx
+++ src/components/CartItem.jsx
@@ -1,5 +1,23 @@
-const CartItem = () => {
-  return <div>CartItem</div>;
+import { AiFillDelete } from "react-icons/ai";
+
+const CartItem = ({ item, itemIndex }) => {
+  return (
+    <div>
+      <div>
+        <img src={item.image} alt="" />
+      </div>
+      <div>
+        <h1>{item.title}</h1>
+        <h2>{item.description}</h2>
+        <div>
+          <p>{item.price}</p>
+          <div>
+            <AiFillDelete />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default CartItem;
