345660ed5404843b85872ef470a5850ba44a2927
Prachi24599 <prachi24599@gmail.com>
2023-09-11 13:35:48 +0530

    toast change

--- src/index.js
+++ src/index.js
@@ -5,15 +5,14 @@ import "./index.css";
 import { Provider } from "react-redux";
 import { store } from "./redux/Store";
 import { BrowserRouter } from "react-router-dom";
-import { ToastContainer } from "react-toastify";
+import { Toaster } from "react-hot-toast";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
   <BrowserRouter>
-    <ToastContainer>
-      <Provider store={store}>
-        <App />
-      </Provider>
-    </ToastContainer>
+    <Provider store={store}>
+      <App />
+      <Toaster />
+    </Provider>
   </BrowserRouter>
 );
