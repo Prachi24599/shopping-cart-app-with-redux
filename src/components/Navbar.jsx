5649bd0c7d663423598918604edcd5a744528386
Prachi24599 <prachi24599@gmail.com>
2023-09-11 14:28:01 +0530

    navbar style added

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,27 +1,35 @@
 import { FaShoppingCart } from "react-icons/fa";
 import { NavLink } from "react-router-dom";
 import logo from "../assets/logo.png";
+import { useSelector } from "react-redux";
 const Navbar = () => {
+  const { cart } = useSelector((state) => state);
+
   return (
-    <div className="flex flex-row justify-between">
+    <nav className="flex justify-between items-center h-20 max-w-6xl mx-auto">
       <NavLink to="/">
-        <div>
-          <img src={logo} alt="logo" />
+        <div className="ml-5">
+          <img src={logo} alt="logo" className="h-14" />
         </div>
       </NavLink>
-      <div>
+      <div className="flex items-center font-medium text-slate-100 mr-5 space-x-6">
         <NavLink to="/">
           <div>
             <p>Home</p>
           </div>
         </NavLink>
         <NavLink to="/cart">
-          <div>
-            <FaShoppingCart />
+          <div className="relative">
+            <FaShoppingCart className="text-2xl" />
+            {cart.length > 0 && (
+              <span className="absolute -top-1 -right-2 bg-green-600 text-xs w-5 h-5 rounded-full flex items-center justify-center animate-bounce text-white">
+                {cart.length}
+              </span>
+            )}
           </div>
         </NavLink>
       </div>
-    </div>
+    </nav>
   );
 };
 
