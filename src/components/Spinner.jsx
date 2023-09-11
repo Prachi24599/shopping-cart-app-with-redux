8bc257c36a9eefada6a9d89348c65f6ce0ef9f5f
Prachi24599 <prachi24599@gmail.com>
2023-09-11 22:22:14 +0530

    spinner position fixed

--- src/components/Spinner.jsx
+++ src/components/Spinner.jsx
@@ -2,7 +2,9 @@ import React from "react";
 import "./Spinner.css";
 
 const Spinner = () => {
-  return <div className="spinner"></div>;
+  return (
+    <div className="spinner w-full max-h-full flex justify-center items-center"></div>
+  );
 };
 
 export default Spinner;
