eceae321696edad84e03b0c58ee61bb5f182dcd8
Prachi24599 <prachi24599@gmail.com>
2023-09-11 12:52:10 +0530

    Add and remove button created

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -1,4 +1,20 @@
+import { useDispatch, useSelector } from "react-redux";
+import { add, remove } from "../redux/Slices/cartSlice";
+import { toast } from "react-hot-toast";
+
 const Product = ({ post }) => {
+  const { cart } = useSelector((state) => state);
+  const dispatch = useDispatch();
+
+  const addToCart = () => {
+    dispatch(add(post));
+    toast.success("Item added to Cart");
+  };
+
+  const removeFromCart = () => {
+    dispatch(remove(post.id));
+    toast.success("Item removed from Cart");
+  };
   return (
     <div>
       <div>
@@ -13,7 +29,11 @@ const Product = ({ post }) => {
       <div>
         <p>{post.price}</p>
       </div>
-      <button>{true ? <p>Remove Item</p> : <p>Add to Cart</p>}</button>
+      {cart.some((p) => p.id === post.id) ? (
+        <button onClick={removeFromCart}>Remove Item</button>
+      ) : (
+        <button onClick={addToCart}>Add to Cart</button>
+      )}
     </div>
   );
 };
